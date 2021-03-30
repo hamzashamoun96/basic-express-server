013a8d65891a5ffbb2bc43ae4e3a4e311117ebea
hamza shamoun <76407512+hamzashamoun96@users.noreply.github.com>
2021-03-30 21:43:20 +0300

    Update validator.test.js

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,18 +1,17 @@
 'use strict';
-const valid = require('../src/middleware/validator')
+const superTest = require('supertest');
+const server = require('../src/server.js');
+const request = superTest(server.app);
 
-describe('Validation Middleware',()=>{
-    const next = jest.fn()
-    let req1 = {query:{"name":undefined}};
-    let req2 = {query:{"name":"hamza"}}
-    let res = {}
- 
-    it('There is name property',()=>{
-        valid(req1,res,next);
-        expect(next).toHaveBeenCalledWith('error'); 
-    })
-    it('name is string',()=>{
-        valid(req2,res,next);
-        expect(next).toHaveBeenCalledWith(); 
-    })
-})
+describe('Validator Middleware Test', () => {
+    it('There is no name property', async () => {
+      const response = await request.get('/person');
+      expect(response.status).toEqual(500);
+    });
+  
+    it('There is name property', async () => {
+      const response = await request.get('/person?name=hamza');
+      expect(response.status).toEqual(200);
+      expect(response.body.name).toEqual('hamza');
+    });
+  });
